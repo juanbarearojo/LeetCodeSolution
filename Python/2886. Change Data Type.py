b6ebbee901154a20caa407d7c71e4e96c873c712
barearojo <barearojojuan@gmail.com>
2024-04-26 13:46:39 +0200

    2886

---
+++ Python/2886. Change Data Type.py
@@ -0,0 +1,47 @@
+"""
+DataFrame students
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| student_id  | int    |
+| name        | object |
+| age         | int    |
+| grade       | float  |
++-------------+--------+
+Write a solution to correct the errors:
+
+The grade column is stored as floats, convert it to integers.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+Input:
+DataFrame students:
++------------+------+-----+-------+
+| student_id | name | age | grade |
++------------+------+-----+-------+
+| 1          | Ava  | 6   | 73.0  |
+| 2          | Kate | 15  | 87.0  |
++------------+------+-----+-------+
+Output:
++------------+------+-----+-------+
+| student_id | name | age | grade |
++------------+------+-----+-------+
+| 1          | Ava  | 6   | 73    |
+| 2          | Kate | 15  | 87    |
++------------+------+-----+-------+
+Explanation: 
+The data types of the column grade is converted to int.
+
+
+Si intentas convertir una columna entera de un DataFrame de Pandas a tipo entero con la instrucción students['grade'] = int(students['grade']),
+ te encontrarás con un error. Esto se debe a que no puedes usar la función int() directamente sobre una serie de Pandas de esta manera.
+"""
+
+import pandas as pd
+
+def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+    students['grade'] = students['grade'].astype(int)
+    return students
